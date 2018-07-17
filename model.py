06ce1fec05627a865221f2b83813a6ef5d7f6ad2
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-16 17:50:02 -0700

    created relationships between tables

--- model.py
+++ model.py
@@ -47,9 +47,27 @@ class Rating(db.Model):
     __tablename__ = "ratings"
 
     rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    movie_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     score = db.Column(db.Integer, nullable=False)
+
+    # Define relationship to user
+    user = db.relationship("User",
+                           backref=db.backref("ratings",
+                                              order_by=rating_id))
+
+    # Define relationship to movie
+    movie = db.relationship("Movie",
+                            backref=db.backref("ratings",
+                                               order_by=rating_id))
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return f"""<Rating rating_id={self.rating_id}
+                    movie_id={self.movie_id}
+                    user_id={self.user_id}
+                    score={self.score}>"""
 
 
 ##############################################################################
